cfec29ec80cc32e6bf448515f84fef7363b9f7d1
Valentin Rep <valentin.rep@gmail.com>
2019-03-27 01:20:06 +0100

    before mogration to AndroidX

--- app/src/main/java/hr/tvz/android/MainActivity.kt
+++ app/src/main/java/hr/tvz/android/MainActivity.kt
@@ -2,11 +2,20 @@ package hr.tvz.android
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+//import butterknife.ButterKnife
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        ButterKnife.bind(this);
+
+        btnActionClear.setOnClickListener {
+//            txtResult.text = ""
+        }
     }
+
+
 }
